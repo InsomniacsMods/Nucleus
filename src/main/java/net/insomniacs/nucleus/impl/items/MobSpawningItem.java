8dd29524e0122e5c70bd42850953c0daf942df6c
Soumeh <soumeh@proton.me>
2023-12-16 14:24:52 +0100

    mob spawning item thing

--- src/main/java/net/insomniacs/nucleus/impl/items/MobSpawningItem.java
+++ src/main/java/net/insomniacs/nucleus/impl/items/MobSpawningItem.java
@@ -1,6 +1,7 @@
 package net.insomniacs.nucleus.impl.items;
 
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SpawnReason;
@@ -18,30 +19,34 @@ import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class MobSpawningItem extends Item {
 
     private final EntityType<?> entity;
+    private final Random random;
 
     public EntityType<?> getEntityType(ItemStack stack) {
         NbtCompound nbt = stack.getOrCreateNbt();
         NbtCompound compound = getEntityNbt(nbt);
+        System.out.println(compound);
         if (!compound.contains("id")) return this.entity;
 
         return EntityType.get(compound.getString("id")).orElse(this.entity);
     }
 
     public static NbtCompound getEntityNbt(NbtCompound nbt) {
-        if (!nbt.contains("entity_tag")) return new NbtCompound();
-        return nbt.getCompound("entity_tag");
+        if (!nbt.contains("EntityTag")) return new NbtCompound();
+        return nbt.getCompound("EntityTag");
     }
 
-    public static int SPAWN_RADIUS = 50;
+    public static int SPAWN_RADIUS = 5;
 
     public MobSpawningItem(Settings settings, @Nullable EntityType<?> entity) {
         super(settings);
         this.entity = entity;
+        this.random = new Random();
     }
 
     @Override
@@ -52,16 +57,21 @@ public class MobSpawningItem extends Item {
         if (world.isClient) return finished;
 
         BlockPos pos = entity.getBlockPos();
-        int x = ThreadLocalRandom.current().nextInt(0, SPAWN_RADIUS + 1);
-        int z = SPAWN_RADIUS - x;
-        pos.add(x, 0, z);
-        // TODO maybe make the radius an actual circle instead of whatever the hell this is
+        double angle = Math.toRadians( this.random.nextInt(360) );
+        double x = Math.cos(angle) * SPAWN_RADIUS;
+        double y = 0;
+        double z = Math.sin(angle) * SPAWN_RADIUS;
+        // TODO make y the closest spawnable block
+        pos = pos.add((int)x, (int)y, (int)z);
 
-        EntityType<?> spawnedEntity = getEntityType(stack);
-        spawnedEntity.spawnFromItemStack((ServerWorld)world, stack, (PlayerEntity)entity, pos, SpawnReason.SPAWN_EGG, false, false);
+        EntityType<?> type = getEntityType(stack);
+        Entity spawnedEntity = type.spawnFromItemStack((ServerWorld)world, stack, (PlayerEntity)entity, pos, SpawnReason.SPAWN_EGG, true, false);
+        postSpawn(world, pos, spawnedEntity);
 
         return finished;
     }
+
+    public void postSpawn(World world, BlockPos pos, Entity entity) {};
 
     private Text getEntityName(ItemStack stack) {
         NbtCompound nbt = stack.getOrCreateNbt();
--- src/main/java/net/insomniacs/nucleus/impl/items/NucleusItems.java
+++ src/main/java/net/insomniacs/nucleus/impl/items/NucleusItems.java
@@ -3,6 +3,7 @@ package net.insomniacs.nucleus.impl.items;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.insomniacs.nucleus.Nucleus;
 import net.minecraft.entity.EntityType;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -12,7 +13,7 @@ public class NucleusItems {
     public static final Item LOCATION_BINDING_ITEM = registerItem("location_binding_item",
             new LocationBindingItem(new FabricItemSettings().maxCount(1)));
     public static final Item MOB_SPAWNING_ITEM = registerItem("mob_spawning_item",
-            new MobSpawningItem(new FabricItemSettings().maxCount(1), EntityType.ZOMBIE));
+            new MobSpawningItem(new FabricItemSettings().maxCount(1).food(new FoodComponent.Builder().snack().build()), EntityType.ZOMBIE));
 
     public static void init() {}
 
